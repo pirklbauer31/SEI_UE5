cada748027d381c3f93ca4d2869cfb95f0330b98
Student <Student@VM-macOS-1012.local>
2017-01-30 13:45:22 +0100

    Initial Commit

---
+++ UE5/Character.swift
@@ -0,0 +1,21 @@
+//
+//  Character.swift
+//  UE5
+//
+//  Created by Student on 30/01/2017.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import Foundation
+
+class Character{
+    var name: Int?
+    var height: Double?
+    var mass: Int?
+    var hairColor: String?
+    var skinColor: String?
+    var gender: String?
+    
+    
+}
+
